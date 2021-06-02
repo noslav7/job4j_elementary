a5efc69036039ce0f073592d19a33874e96a6707
noslav7 <noslav7@gmail.com>
2021-06-02 14:37:04 +0500

    4.1. Аргументы [#295521 #163324]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -4,6 +4,7 @@ public class ArgMethod {
 
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
+        System.out.println(age);
     }
 
     public static void main(String[] args) {
