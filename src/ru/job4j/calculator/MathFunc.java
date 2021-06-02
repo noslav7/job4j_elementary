a836b73078bcc68ea8a43fead871897d31791167
noslav7 <noslav7@gmail.com>
2021-06-02 14:47:25 +0500

    4.2. Результат работы метода [#232622 #163336]

--- src/ru/job4j/calculator/MathFunc.java
+++ src/ru/job4j/calculator/MathFunc.java
@@ -1,12 +1,15 @@
 package ru.job4j.calculator;
 
+import java.sql.PreparedStatement;
+
 public class MathFunc {
     public static int func1(int x) {
         int result = x * 100;
+        System.out.println(result);
         return result;
     }
 
     public static void main(String[] args) {
-        int x = 100;
+        MathFunc.func1(100);
     }
 }
