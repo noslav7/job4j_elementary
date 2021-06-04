bbf16a6a3fa011452a048e781726ed7c67cad18d
noslav7 <noslav7@gmail.com>
2021-06-04 20:05:22 +0500

    0. Что такое тестирование [#159431]

--- src/ru/job4j/ru/job4j/converter/Converter.java
+++ src/ru/job4j/ru/job4j/converter/Converter.java
@@ -28,29 +28,33 @@ public class Converter {
 
     public static void main(String[] args) {
 
-        float rTeExpected = 2.0f;
-        float rTdExpected = 2.8333333f;
-        float eTrExpected = 3500.0f;
-        float dTrExpected = 6000.0f;
-        float eTdExpected = 116.666664f;
-        float dTeExpected = 171.42857f;
-
         float rub4euro = Converter.rubleToEuro(140f);
+        float rTeExpected = 2.0f;
         boolean rubEuro = rTeExpected == rub4euro;
         System.out.println("140 rubles are " + rub4euro + " euro. Test result : " + rubEuro);
+
         float rub4dollar = Converter.rubleToDollar(170f);
+        float rTdExpected = 2.8333333f;
         boolean rubDollar = rTdExpected == rub4dollar;
         System.out.println("160 rubles are " + rub4dollar + " USD. Test result : " + rubDollar);
+
         float euro4ruble = Converter.euroToRuble(50f);
+        float eTrExpected = 3500.0f;
         boolean euroRuble = eTrExpected == euro4ruble;
         System.out.println("50 euro are " + euro4ruble + " rubles. Test result : " + euroRuble);
+
         float dollar4ruble = Converter.dollarToRuble(100f);
+        float dTrExpected = 6000.0f;
         boolean dollarRuble = dTrExpected == dollar4ruble;
         System.out.println("100 USD are " + dollar4ruble + " rubles. Test result : " + dollarRuble);
+
         float euro4dollar = Converter.euroToDollar(100f);
+        float eTdExpected = 116.666664f;
         boolean euroDollar = eTdExpected == euro4dollar;
         System.out.println("100 euro are " + euro4dollar + " USD. Test result : " + euroDollar);
+
         float dollar4euro = Converter.dollarToEuro(200f);
+        float dTeExpected = 171.42857f;
         boolean dollarEuro = dTeExpected == dollar4euro;
         System.out.println("200 USD are " + dollar4euro + " euro. Test result : " + dollarEuro);
     }
