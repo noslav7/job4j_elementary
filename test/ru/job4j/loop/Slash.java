72163c59dbe44069cdb611b3ba6c97f06c5fc13b
noslav7 <noslav7@gmail.com>
2021-06-11 22:00:29 +0500

    5.9.1. Генерация конструкций в IDEA [#279134 #166747]

--- test/ru/job4j/loop/Slash.java
+++ test/ru/job4j/loop/Slash.java
@@ -4,8 +4,8 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = (row == 0 && cell == 0) || (row == 1 && cell == 1) || (row == 2 && cell == 2) || (row == 3 && cell == 3) || (row == 4 && cell == 4);
-                boolean right = (row == size - 1 && cell == 0) || (row == size - 2 && cell == 1) || (row == size - 3 && cell == 2) || (row == size - 4 && cell == 3) || (row == size - 5 && cell == 4);
+                boolean left = (row == cell);
+                boolean right = (row + cell == size - 1);
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
