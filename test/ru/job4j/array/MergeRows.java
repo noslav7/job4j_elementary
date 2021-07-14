42a888508a59ae878a78401fd8763c988db0f651
noslav7 <noslan7@gmail.com>
2021-07-14 12:06:58 +0500

    Exercises. Arrays

---
+++ test/ru/job4j/array/MergeRows.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+import java.util.stream.Stream;
+
+public class MergeRows {
+    public static int[] merge(int[][] data) {
+        Stream<Integer> concatenated = Stream.concat(
+
+        );
+            }
+        }
+    }
+}
