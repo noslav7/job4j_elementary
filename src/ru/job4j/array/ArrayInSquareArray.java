d2f886d4ec3dafcdd12b23e16e756cccb6a216d5
noslav7 <noslav7@gmail.com>
2021-08-05 21:07:11 +0500

    Exercises Arrays

--- src/ru/job4j/array/ArrayInSquareArray.java
+++ src/ru/job4j/array/ArrayInSquareArray.java
@@ -2,10 +2,14 @@ package ru.job4j.array;
 
 public class ArrayInSquareArray {
     public static int[][] convertArray(int[] array) {
-        int[][] twoDArray = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                twoDArray[i * 3 + j][j] = array[i];
+        int length = 1;
+        while (array.length > length * length) {
+            length++;
+        }
+        int[][] twoDArray = new int[length][length];
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                twoDArray[i][j] = array[i * twoDArray.length + j - 1];
             }
         }
         return twoDArray;
