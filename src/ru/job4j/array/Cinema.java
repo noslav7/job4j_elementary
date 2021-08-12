fb1fc0b183f317ccf9c614095e38e6d3dd662b9f
noslav7 <noslav7@gmail.com>
2021-08-12 21:39:22 +0500

    Exercises Arrays

--- src/ru/job4j/array/Cinema.java
+++ src/ru/job4j/array/Cinema.java
@@ -4,6 +4,18 @@ import java.util.Objects;
 
 public class Cinema {
     public static Place checkEmptyPlace(Place[][] places) {
+        for (int i = 0; i < places.length; i++) {
+            for (int z = 0; z < places[i].length; z++) {
+                if (places[i][z] != null && z + 2 < places[i].length) {
+                    z = z + 1;
+                    continue;
+                } else if (places[i][z] == null && z + 1 < places[i].length && places[i][z + 1] == null
+                        && i + 1 < places.length && places[i + 1][z] == null) {
+                    Place place  = new Place(i, z);
+                    return place;
+                }
+            }
+        }
         return null;
     }
 
@@ -26,8 +38,8 @@ public class Cinema {
                 return false;
             }
             Place place = (Place) o;
-            return row == place.row &&
-                    cell == place.cell;
+            return row == place.row
+                    && cell == place.cell;
         }
 
         @Override
--- src/ru/job4j/array/OrArray.java
+++ src/ru/job4j/array/OrArray.java
@@ -1,34 +1,40 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class OrArray {
-
+/*
     public static int[] or(int[] left, int[] right) {
         int foundEqual = 0;
-        for (int i = 0; i < left.length; i++) {
-            for (int j = 0; j < right.length; j++) {
-                if (left[i] == right[j]) {
-                    foundEqual++;
-                    break;
+        if (left.length != 0) {
+            for (int i = 0; i < left.length; i++) {
+                for (int j = 0; j < right.length; j++) {
+                    if (left[i] == right[j]) {
+                        foundEqual++;
+                    }
                 }
             }
         }
         int[] unitedElements = new int[left.length + right.length - foundEqual];
-        for (int i = 0; i < left.length; i++) {
-            unitedElements[i] = left[i];
+        if (left.length != 0) {
+            for (int i = 0; i < left.length; i++) {
+                unitedElements[i] = left[i];
+            }
+        } else {
+            for (int i = 0; i < right.length; i++) {
+                unitedElements[i] = right[i];
+            }
         }
         int cell = 0;
-        for (int i = 0; i < left.length; i++) {
-            for (int j = 0; j < right.length; j++) {
-                if (left[i] != right[j]) {
-                    unitedElements[left.length + cell] = right[j];
-                    cell++;
+        if (left.length != 0) {
+            for (int i = 0; i < left.length; i++) {
+                for (int j = 0; j < right.length; j++) {
+                    if (left[i] != right[j]) {
+                        unitedElements[left.length + cell] = right[i * left.length + j];
+                        cell++;
+                    }
                 }
             }
         }
         return unitedElements;
-    }
+    }*/
 }
 
