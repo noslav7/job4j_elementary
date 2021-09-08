d82e38df23def4807bfe1d3812bc127a0e0fe7d3
Valentin Rachkov <noslav7@gmail.com>
2021-09-08 20:39:14 +0500

    Exercises Arrays

---
+++ src/ru/job4j/array/Anomaly.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Anomaly {
+    public static int[][] found(int[] data, int up, int down) {
+        int[][] result = new int[data.length][];
+        int[] temp = new int[2];
+        int j = 0;
+        int count = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] >= up || data[i] <= down) {
+                temp[j++] = i;
+            }
+            if (j == 2) {
+                result[count++] = temp;
+                    j = 0;
+            }
+        }
+        if (j == 1 && count == 0) {
+            temp = new int[2];
+            temp[1] = temp[0];
+            result[0] = temp;
+            count++;
+        }
+        return Arrays.copyOf(result, count);
+    }
+}
--- src/ru/job4j/array/SymmetricDiff.java
+++ src/ru/job4j/array/SymmetricDiff.java
@@ -1,12 +1,8 @@
 package ru.job4j.array;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class SymmetricDiff {
-
+/*
     public static int[] diff(int[] left, int[] right) {
 
-    }
+    }*/
 }
