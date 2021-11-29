4fe639a0d6b5fd88881e1979b40d0ebf047c415c
Valentin Rachkov <noslav7@gmail.com>
2021-11-29 22:16:27 +0500

    Exercises Arrays

--- src/ru/job4j/array/Split.java
+++ src/ru/job4j/array/Split.java
@@ -6,14 +6,11 @@ public class Split {
     public static char[][] split(char[] str, char c) {
         char[][] result = new char[str.length / 2][];
         int commonCount = 1;
-        int index = 0;
         for (int i = 0; i < result.length; i++) {
+            int count = 0;
             for (int j = 0; j < str.length; j++) {
-                int ri = 0;
                 if (str[j] != c) {
-                    result[i][ri++] = str[j];
-                } else if (str[j] == c) {
-                    index = j + 1;
+                    result[i][count++] = str[j];
                     commonCount++;
                 }
             }
