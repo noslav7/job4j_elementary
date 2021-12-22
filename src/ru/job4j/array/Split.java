64f56c1871a61d2581fc03c2821c6e7b84c29a52
noslav7 <noslan7@gmail.com>
2021-12-22 10:11:56 +0500

    Exercises. Arrays

--- src/ru/job4j/array/Split.java
+++ src/ru/job4j/array/Split.java
@@ -6,15 +6,15 @@ public class Split {
     public static char[][] split(char[] str, char c) {
         char[][] result = new char[str.length / 2][];
         int commonCount = 1;
-        for (int i = 0; i < result.length; i++) {
-            int count = 0;
-            for (int j = 0; j < str.length; j++) {
-                if (str[j] != c) {
-                    result[i][count++] = str[j];
-                    commonCount++;
+        int ri = 0;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == c) {
+                continue;
+            } else {
+                result[ri++] = new String(str[c != 0 i : continue]);
                 }
+            commonCount++;
             }
-        }
         return Arrays.copyOf(result, commonCount);
     }
 }
