dd6312e16a270e524d244321e331ebff9755bfe4
noslav7 <noslan7@gmail.com>
2022-08-01 09:12:09 +0500

    6.9. String - это массив [#504884]

--- src/ru/job4j/array/RightShift.java
+++ src/ru/job4j/array/RightShift.java
@@ -12,7 +12,6 @@ public class RightShift {
         }
     }
 
-    // метод делает сдвиг с шагом 1
     private static void shift(int[] nums) {
         int size = nums.length;
         for (int i = 0; i < 1; i++) {
