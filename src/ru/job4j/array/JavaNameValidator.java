f363cdf85645c0c2c8c377e06771e90c256b7eba
noslav7 <noslan7@gmail.com>
2022-08-03 09:43:44 +0500

    6.9. String - это массив [#504884]

--- src/ru/job4j/array/JavaNameValidator.java
+++ src/ru/job4j/array/JavaNameValidator.java
@@ -3,10 +3,12 @@ package ru.job4j.array;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         boolean valid = true;
-        if (Character.isLetter(name.charAt(0)) && Character.isLowerCase(name.charAt(0))) {
-            char[] nameChars = name.toCharArray();
-            for (int i = 0; i < nameChars.length; i++) {
-                if (!isSpecialSymbol(i) || isUpperLatinLetter(i) || isLowerLatinLetter(i)) {
+        char[] nameChars = name.toCharArray();
+        if ((isSpecialSymbol(nameChars[0]) || isLowerLatinLetter(nameChars[0]))
+                && name.charAt(0) != Character.MIN_VALUE) {
+            for (int i = 1; i < nameChars.length; i++) {
+                if (!(isSpecialSymbol(nameChars[i]) || isUpperLatinLetter(nameChars[i])
+                        || isLowerLatinLetter(nameChars[i]))) {
                     valid = false;
                     break;
                 }
@@ -18,14 +20,26 @@ public class JavaNameValidator {
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return false;
+        boolean specialSymbol = false;
+        if (code == 36 || code == 95) {
+            specialSymbol = true;
+        }
+        return specialSymbol;
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return false;
+        boolean upperLatinLetter = false;
+        if (code >= 65 && code <= 90) {
+            upperLatinLetter = true;
+        }
+        return upperLatinLetter;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return false;
+        boolean lowerLatinLetter = false;
+        if (code >= 97 && code <= 122) {
+            lowerLatinLetter = true;
+        }
+        return lowerLatinLetter;
     }
 }
