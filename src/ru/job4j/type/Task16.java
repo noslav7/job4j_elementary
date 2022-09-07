d8bba51143a49285b9d0c07a47a00aa696fc3b9c
noslav7 <noslan7@gmail.com>
2022-09-07 09:40:00 +0500

    exercises

---
+++ src/ru/job4j/type/Task16.java
@@ -0,0 +1,13 @@
+package ru.job4j.type;
+
+public class Task16 {
+    public static void decreaseBy2(int number) {
+        int a, b, c;
+        a = number - 1;
+        b = a - 1;
+        c = b - 1;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
