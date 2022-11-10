639db51715691ae8e9bd8c7d47260866bd93d672
noslav7 <noslan7@gmail.com>
2022-11-10 09:36:12 +0500

    exercises

--- src/ru/job4j/loop/Task120.java
+++ src/ru/job4j/loop/Task120.java
@@ -2,21 +2,13 @@ package ru.job4j.loop;
 
 public class Task120 {
     public static void loop(int num) {
-        StringBuilder results = new StringBuilder();
-        String digitString = "";
-        int count = 0;
-        int lastNumber = 0;
         while (num != 0) {
-            lastNumber = num % 10;
-            digitString = digitString + String.valueOf(lastNumber);
-            count++;
-            if (count == 1) {
-              results.append(digitString);
-            } else {
-                results.append(" ").append(digitString);
-            }
             num /= 10;
+            if (num != 0) {
+                System.out.print(num + " ");
+            } else {
+                System.out.println(num);
+            }
         }
-        System.out.println(results);
     }
 }
