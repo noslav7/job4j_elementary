32778091281f391e7c46a591c0cf77d9e2f874be
noslav7 <noslav7@gmail.com>
2022-11-11 18:14:42 +0500

    exercises

--- src/ru/job4j/loop/Task123.java
+++ src/ru/job4j/loop/Task123.java
@@ -4,6 +4,10 @@ import java.util.StringJoiner;
 
 public class Task123 {
     public static void loop(int num) {
+        if (num < 10) {
+            System.out.print("");
+            return;
+        }
         StringJoiner joiner = new StringJoiner(" ");
         int sum = 0;
         while (num != 0) {
@@ -14,6 +18,9 @@ public class Task123 {
         joiner.add(String.valueOf(sum));
         int newNumberSum = sum;
         while (newNumberSum != 0) {
+            if (newNumberSum < 10) {
+                break;
+            }
             int singleSum = 0;
             while (newNumberSum != 0) {
                 int remains = newNumberSum % 10;
@@ -21,12 +28,11 @@ public class Task123 {
                 newNumberSum /= 10;
             }
             joiner.add(String.valueOf(singleSum));
-            newNumberSum = singleSum;
         }
         System.out.println(joiner);
     }
 
     public static void main(String[] args) {
-        loop(12345);
+        loop(6);
     }
 }
