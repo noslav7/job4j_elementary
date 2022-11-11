75972a84724868f9dae62d40f14bd088a5eceeec
noslav7 <noslan7@gmail.com>
2022-11-11 12:50:39 +0500

    exercises

--- src/ru/job4j/loop/Task121.java
+++ src/ru/job4j/loop/Task121.java
@@ -4,22 +4,17 @@ import java.util.StringJoiner;
 
 public class Task121 {
     public static void loop(int[] nums) {
-        StringJoiner joiner = new StringJoiner(", ");
+        StringJoiner joiner = new StringJoiner(" ");
         for (int i = 0; i < nums.length; i++) {
             int sum = 0;
             int number = nums[i];
             while (number != 0) {
                 int remains = number % 10;
-                sum = sum + number;
+                sum = sum + remains;
                 number = number / 10;
             }
             joiner.add(String.valueOf(sum));
         }
         System.out.println(joiner);
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {10, 20, 31};
-        loop(nums);
     }
 }
--- src/ru/job4j/loop/Task122.java
+++ src/ru/job4j/loop/Task122.java
@@ -1,7 +1,31 @@
 package ru.job4j.loop;
 
+import java.util.StringJoiner;
+
 public class Task122 {
     public static void loop(int[] nums) {
-        //...Ваш код...;
+        StringJoiner joiner = new StringJoiner(" ");
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            int number = nums[i];
+            count++;
+            while (number != 0) {
+                int remains = number % 10;
+                sum = sum + remains;
+                number = number / 10;
+            }
+            if (sum == 5) {
+                if (count == 1) {
+                    System.out.print("");
+                    return;
+                } else {
+                    System.out.println(joiner);
+                    return;
+                }
+            }
+         joiner.add(String.valueOf(nums[i]));
+            }
+            System.out.println(joiner);
+        }
     }
-}
