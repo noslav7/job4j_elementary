d210ec037c45c1883f1f739006338fa1e57f2cfa
noslav7 <noslan7@gmail.com>
2022-11-14 11:43:21 +0500

    exercises

--- src/ru/job4j/loop/Task131.java
+++ src/ru/job4j/loop/Task131.java
@@ -1,7 +1,21 @@
 package ru.job4j.loop;
 
+import java.util.StringJoiner;
+
 public class Task131 {
     public static void loop(int[] nums) {
-        //...Ваш код...;
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        int countEven = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                countEven++;
+            }
+            if (countEven < 3) {
+                stringJoiner.add(String.valueOf(nums[i]));
+            } else {
+                break;
+            }
+        }
+        System.out.println(stringJoiner);
     }
 }
