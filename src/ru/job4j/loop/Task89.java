7c5afeabb21cc4e89346b73e0a31c903bcc506d1
noslav7 <noslan7@gmail.com>
2022-11-28 13:19:53 +0500

    exercises

--- src/ru/job4j/loop/Task89.java
+++ src/ru/job4j/loop/Task89.java
@@ -3,11 +3,12 @@ package ru.job4j.loop;
 public class Task89 {
     public static void loop() {
         int count = 0;
-        for (int i = 0; i <= 999; i++) {
+        for (int i = 2; i <= 999; i++) {
             int firstDigit = i / 100;
             int secondDigit = i % 100 / 10;
             int thirdDigit = i % 10;
-            double cube = Math.pow((double) (firstDigit + secondDigit + thirdDigit), 3.0);
+            double cube = Math.pow((double) firstDigit, 3.0)
+                    + Math.pow((double) secondDigit, 3.0)  + Math.pow((double) thirdDigit, 3.0);
             if (cube == (double) i) {
                 count++;
                 if (count == 1) {
