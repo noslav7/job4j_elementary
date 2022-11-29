46b395e7b972555a6f07b97ac8fd2d09af8d937c
noslav7 <noslav7@gmail.com>
2022-11-29 12:14:34 +0500

    exercises

--- src/ru/job4j/array/Task79.java
+++ src/ru/job4j/array/Task79.java
@@ -5,6 +5,7 @@ import java.util.StringJoiner;
 public class Task79 {
     public static void array(int[] nums) {
         StringJoiner stringJoiner = new StringJoiner(" ");
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < nums.length; i++) {
             int count = 0;
             for (int j = 0; j < nums.length; j++) {
@@ -12,8 +13,8 @@ public class Task79 {
                     count++;
                 }
             }
-            if (count > 1) {
-
+            if (count > 1 && !stringJoiner.toString().contains(String.valueOf(nums[i]))) {
+                    stringJoiner.add(String.valueOf(nums[i]));
             }
         }
         if (stringJoiner.length() == 0) {
