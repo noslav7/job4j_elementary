d2afcc67131de7a4788a675962679cd3dfdb397a
noslav7 <noslav7@gmail.com>
2023-01-08 11:32:40 +0500

    exercises

--- src/ru/job4j/array/Task75.java
+++ src/ru/job4j/array/Task75.java
@@ -35,34 +35,48 @@ public class Task75 {
             }
         }
 
-        int max = Math.max(count1, Math.max(count3, Math.max(count4, Math.max(count5, Math.max(count6,
-                Math.max(count7, Math.max(count8, count9)))))));
-        int min = Math.min(count1, Math.max(count3, Math.max(count4, Math.max(count5, Math.max(count6,
-                Math.max(count7, Math.max(count8, count9)))))));
+        int[] counts = new int[] {0, count1, count2, count3, count4, count5, count6, count7, count8, count9};
 
-        StringJoiner stringJoinerMax = new StringJoiner(" ");
-        StringJoiner stringJoinerMin = new StringJoiner(" ");
-        StringBuilder stringBuilderMiddle = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == max) {
-                stringJoinerMax.add(String.valueOf(arr[i]));
-            } else if (arr[i] == min) {
-                stringJoinerMin.add(String.valueOf(arr[i]));
-            } else {
-                stringBuilderMiddle.append(arr[i]);
+        int maxCount = 0;
+        int minCount = 10;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > maxCount) {
+                maxCount = counts[i];
+            }
+            if (counts[i] < minCount && counts[i] != 0) {
+                minCount = counts[i];
             }
         }
 
+        StringJoiner stringJoinerMax = new StringJoiner(" ");
+        StringJoiner stringJoinerMin = new StringJoiner(" ");
         StringJoiner stringJoinerNotExist = new StringJoiner(" ");
-        for (int i = 1; i < 10; i++) {
-            if (!stringBuilderMiddle.toString().contains(String.valueOf(i)) && i != max && i != min) {
+        for (int i = 1; i < counts.length; i++) {
+            if (counts[i] == maxCount && maxCount != minCount) {
+                stringJoinerMax.add(String.valueOf(i));
+            } else if (counts[i] == minCount && maxCount != minCount) {
+                stringJoinerMin.add(String.valueOf(i));
+            } else if (counts[i] == 0){
                 stringJoinerNotExist.add(String.valueOf(i));
             }
+        }
+
+        if (stringJoinerMax.toString().isEmpty()) {
+            stringJoinerMax.add(String.valueOf(0));
+        }
+
+        if (stringJoinerMin.toString().isEmpty()) {
+            stringJoinerMin.add(String.valueOf(0));
         }
 
         System.out.println("1: " + count1 + ", 2: " + count2 + ", 3: " + count3 + ", 4: " + count4 + ", 5: " + count5
                 + ", 6: " + count6 + ", 7: " + count7 + ", 8: " + count8 + ", 9: " + count9);
         System.out.println("Чаще: " + stringJoinerMax + ", отсутствует: " + stringJoinerNotExist
                 + ", реже: " + stringJoinerMin);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1, 2};
+        array(arr);
     }
 }
