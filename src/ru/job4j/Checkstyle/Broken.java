49e0cdd1527fa02318e516a2b422a71dcb856ec6
noslav7 <noslan7@gmail.com>
2023-01-12 13:14:17 +0500

    checkstyle.xml

---
+++ src/ru/job4j/Checkstyle/Broken.java
@@ -0,0 +1,30 @@
+package ru.job4j.Checkstyle;
+
+public class Broken {
+    private int size_of_empty = 10;
+
+    public String surname;
+
+
+    public static final String newValue = "";
+
+    void echo() { }
+
+
+    void media (Object obj) {
+        if (obj != null)
+            System.out.println( obj );
+    }
+
+
+
+    void method(int a, int b, int c, int d, int e, int f, int g, int h)
+    {
+
+    }
+
+
+    String name;
+
+    Broken() {}
+}
